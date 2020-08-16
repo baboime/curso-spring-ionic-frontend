f60da2dbf022c4a2b2508ad0dd1374a03412492c
Edney Baboim <ednney@gmail.com>
2020-08-16 17:03:51 -0300

    Salvando novo cliente

--- src/interceptors/error-interceptor.ts
+++ src/interceptors/error-interceptor.ts
@@ -3,6 +3,7 @@ import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HTTP_INTERCEPTORS
 import { Observable } from 'rxjs/Rx'; // IMPORTANTE: IMPORT ATUALIZADO
 import { StorageService } from '../services/storage.service';
 import { AlertController } from 'ionic-angular';
+import { FieldMessage } from '../models/fieldmessage'
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
@@ -31,6 +32,8 @@ export class ErrorInterceptor implements HttpInterceptor {
                 break;
                 case 403: this.handle403();
                 break;
+                case 422: this.handle422(errorObj);
+                break;
 
                 default: this.handleDefaultError(errorObj);
             }
@@ -57,6 +60,21 @@ export class ErrorInterceptor implements HttpInterceptor {
         alert.present();
     }
 
+    handle422(errorObj) {
+        let alert = this.alertCtrl.create({
+            title: 'Erro 422: Validação',
+            message: this.listErrors(errorObj.errors),
+            enableBackdropDismiss: false,
+            buttons: [
+                {
+                    text: 'Ok'  
+                }
+            ]
+        });
+        console.log("Chegou no FIM")
+        alert.present();
+    }
+
     handleDefaultError(errorObj) {
         let alert = this.alertCtrl.create({
             title: 'Erro ' + errorObj.status + ': ' + errorObj.error,
@@ -71,6 +89,14 @@ export class ErrorInterceptor implements HttpInterceptor {
         alert.present();
     }
 
+    private listErrors(messages : FieldMessage[]) : string {
+        let s : string = '';
+
+        for (var i=0; i<messages.length; i++) {
+            s = s + '<p><strong>' + messages[i].fieldName + "</strong>: " + messages[i].message + '</p>';
+        }
+        return s;
+    }
 }
 
 export const ErrorInterceptorProvider = {
