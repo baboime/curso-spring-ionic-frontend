721f0a50db64dd4827d418e95b10af6e1d510032
Edney Baboim <ednney@gmail.com>
2020-08-22 22:51:31 -0300

    Tela de escolha de endereço

--- src/models/cidade.dto.ts
+++ src/models/cidade.dto.ts
@@ -1,6 +1,9 @@
 import { asPureExpressionData } from "@angular/core/src/view";
+import { EstadoService } from "../services/domain/estado.service";
+import { EstadoDTO } from "./estado.dto";
 
 export interface CidadeDTO {
     id : string;
     nome : string;
+    estado? : EstadoDTO;
 }
--- src/models/estado.dto.ts
+++ src/models/estado.dto.ts
@@ -1,3 +1,5 @@
+import { EstadoService } from "../services/domain/estado.service";
+
 export interface EstadoDTO {
     id : string;
     nome : string;
